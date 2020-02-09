20b36c00759422444813a0d9e0056459e047889b
HyphenSaad <HyphenSaad@gmail.com>
2020-02-09 12:23:10 +0500

    Updated

--- main.cpp
+++ main.cpp
@@ -1,173 +1,162 @@
-#include "custom.h"
-main()
-{
+#include "include/custom.h"
+main(){
     ////////////////////////////////////////////////////////////////////////////////////////////
     // intro - module
     initwindow(1020, 270, "Welcome! Periodic Table, A Project By HyphenSaad");
     cout << "> Press Enter Key To Continue..." << endl;
-    drawMPTL(75, 75, 10);
+    drawMPTL(75,75,10);
     closegraph(CURRENT_WINDOW);
 
-////////////////////////////////////////////////////////////////////////////////////////////
-// data processing - module
-RERUN:
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    // data processing - module
+    RERUN:
 
-    // local variables
-    string atomicNumber, atomicRadius, atomicWeight, block, boilingPoint, density, electroNegativity,
-        electronicConfiguration, elementSymbol, group, meltingPoint, elementName, input = "";
-    int elementNumber = 0;
-    char option;
+	// local variables
+	string atomicNumber, atomicRadius, atomicWeight, block, boilingPoint, density, electroNegativity,
+	electronicConfiguration, elementSymbol, group, meltingPoint, elementName, input = "";
+	int elementNumber = 0;
+	char option;
 
-NOINPUT:
+	NOINPUT:
 
-    // to get input from files
-    fstream inputDataFileObj;
-    inputDataFileObj.open("input.txt", ios::in);
-    getline(inputDataFileObj, input);
-    inputDataFileObj.close();
+	// to get input from files
+	fstream inputDataFileObj;
+	inputDataFileObj.open("input.txt", ios::in);
+	getline(inputDataFileObj, input);
+	inputDataFileObj.close();
 
-    // to check if input file is empty
-    if (input == "")
-    {
+	// to check if input file is empty
+    if(input == ""){
         cout << "> No input in file..." << endl;
-    A:
+        A:
         cout << "> Enter 0 To Check Again..." << endl;
         cout << "> Enter x To Exit...";
         char check = ' ';
         cin >> check;
-        if (check == '0')
-        {
+        if(check == '0'){
             goto NOINPUT;
-        }
-        else if (check == 'x' || check == 'X')
-        {
+        } else if(check == 'x' || check == 'X'){
             exit(1);
-        }
-        else
-        {
+        } else {
             cout << "> Invalid! Command..." << endl;
             goto A;
         }
     }
 
-    // to find the entered element symbols, location in periodic table
-    fstream byElementSymbolDataFile;
-    byElementSymbolDataFile.open("data/elementSymbol.txt", ios::in);
+	// to find the entered element symbols, location in periodic table
+	fstream byElementSymbolDataFile;
+	byElementSymbolDataFile.open("data/elementSymbol.txt", ios::in);
 
-    for (int i = 0; i < TOTAL_ELEMENTS; i++)
-    {
-        getline(byElementSymbolDataFile, elementSymbol);
-        if (elementSymbol == input)
-        {
-            elementNumber = i + 1;
-        }
-    }
+	for(int i = 0 ; i < TOTAL_ELEMENTS ; i++){
+		getline(byElementSymbolDataFile, elementSymbol);
+		if(elementSymbol == input){
+			elementNumber = i + 1;
+		}
+	}
 
-    byElementSymbolDataFile.close();
+	byElementSymbolDataFile.close();
 
-    // to find the entered atomic number, location in periodic table
-    fstream byAtomicNumberDataFile;
-    byAtomicNumberDataFile.open("data/atomicNumber.txt", ios::in);
+	// to find the entered atomic number, location in periodic table
+	fstream byAtomicNumberDataFile;
+	byAtomicNumberDataFile.open("data/atomicNumber.txt", ios::in);
 
-    for (int i = 0; i < TOTAL_ELEMENTS; i++)
-    {
-        getline(byAtomicNumberDataFile, atomicNumber);
-        if (atomicNumber == input)
-        {
-            elementNumber = i + 1;
-        }
-    }
+	for(int i = 0 ; i < TOTAL_ELEMENTS ; i++){
+		getline(byAtomicNumberDataFile, atomicNumber);
+		if(atomicNumber == input){
+			elementNumber = i + 1;
+		}
+	}
 
-    byAtomicNumberDataFile.close();
+	byAtomicNumberDataFile.close();
 
-    // to get element name
-    fstream elementNameDataFile;
-    elementNameDataFile.open("data/elementName.txt", ios::in);
-    GotoLine(elementNameDataFile, elementNumber);
-    getline(elementNameDataFile, elementName);
-    elementNameDataFile.close();
+	// to get element name
+	fstream elementNameDataFile;
+	elementNameDataFile.open("data/elementName.txt", ios::in);
+	GotoLine(elementNameDataFile, elementNumber);
+	getline(elementNameDataFile, elementName);
+	elementNameDataFile.close();
 
-    // to get atomic number
-    fstream atomicNumberDataFile;
-    atomicNumberDataFile.open("data/atomicNumber.txt", ios::in);
-    GotoLine(atomicNumberDataFile, elementNumber);
-    getline(atomicNumberDataFile, atomicNumber);
-    atomicNumberDataFile.close();
+	// to get atomic number
+	fstream atomicNumberDataFile;
+	atomicNumberDataFile.open("data/atomicNumber.txt", ios::in);
+	GotoLine(atomicNumberDataFile, elementNumber);
+	getline(atomicNumberDataFile, atomicNumber);
+	atomicNumberDataFile.close();
 
-    // to get atomic radius
-    fstream atomicRadiusDataFile;
-    atomicRadiusDataFile.open("data/atomicRadius.txt", ios::in);
-    GotoLine(atomicRadiusDataFile, elementNumber);
-    getline(atomicRadiusDataFile, atomicRadius);
-    atomicRadiusDataFile.close();
+	// to get atomic radius
+	fstream atomicRadiusDataFile;
+	atomicRadiusDataFile.open("data/atomicRadius.txt", ios::in);
+	GotoLine(atomicRadiusDataFile, elementNumber);
+	getline(atomicRadiusDataFile, atomicRadius);
+	atomicRadiusDataFile.close();
 
-    // to get atomic weight
-    fstream atomicWeightDataFile;
-    atomicWeightDataFile.open("data/atomicWeight.txt", ios::in);
-    GotoLine(atomicWeightDataFile, elementNumber);
-    getline(atomicWeightDataFile, atomicWeight);
-    atomicWeightDataFile.close();
+	// to get atomic weight
+	fstream atomicWeightDataFile;
+	atomicWeightDataFile.open("data/atomicWeight.txt", ios::in);
+	GotoLine(atomicWeightDataFile, elementNumber);
+	getline(atomicWeightDataFile, atomicWeight);
+	atomicWeightDataFile.close();
 
-    // to get block
-    fstream blockDataFile;
-    blockDataFile.open("data/block.txt", ios::in);
-    GotoLine(blockDataFile, elementNumber);
-    getline(blockDataFile, block);
-    blockDataFile.close();
+	// to get block
+	fstream blockDataFile;
+	blockDataFile.open("data/block.txt", ios::in);
+	GotoLine(blockDataFile, elementNumber);
+	getline(blockDataFile, block);
+	blockDataFile.close();
 
-    // to get boilingPoint
-    fstream boilingPointDataFile;
-    boilingPointDataFile.open("data/boilingPoint.txt", ios::in);
-    GotoLine(boilingPointDataFile, elementNumber);
-    getline(boilingPointDataFile, boilingPoint);
-    boilingPointDataFile.close();
+	// to get boilingPoint
+	fstream boilingPointDataFile;
+	boilingPointDataFile.open("data/boilingPoint.txt", ios::in);
+	GotoLine(boilingPointDataFile, elementNumber);
+	getline(boilingPointDataFile, boilingPoint);
+	boilingPointDataFile.close();
 
-    // to get density
-    fstream densityDataFile;
-    densityDataFile.open("data/density.txt", ios::in);
-    GotoLine(densityDataFile, elementNumber);
-    getline(densityDataFile, density);
-    densityDataFile.close();
+	// to get density
+	fstream densityDataFile;
+	densityDataFile.open("data/density.txt", ios::in);
+	GotoLine(densityDataFile, elementNumber);
+	getline(densityDataFile, density);
+	densityDataFile.close();
 
-    // to get electronegativity
-    fstream electronegativityDataFile;
-    electronegativityDataFile.open("data/electronegativity.txt", ios::in);
-    GotoLine(electronegativityDataFile, elementNumber);
-    getline(electronegativityDataFile, electroNegativity);
-    electronegativityDataFile.close();
+	// to get electronegativity
+	fstream electronegativityDataFile;
+	electronegativityDataFile.open("data/electronegativity.txt", ios::in);
+	GotoLine(electronegativityDataFile, elementNumber);
+	getline(electronegativityDataFile, electroNegativity);
+	electronegativityDataFile.close();
 
-    // to get electronic configuration
-    fstream electronicConfigurationDataFile;
-    electronicConfigurationDataFile.open("data/electronicConfiguration.txt", ios::in);
-    GotoLine(electronicConfigurationDataFile, elementNumber);
-    getline(electronicConfigurationDataFile, electronicConfiguration);
-    electronicConfigurationDataFile.close();
+	// to get electronic configuration
+	fstream electronicConfigurationDataFile;
+	electronicConfigurationDataFile.open("data/electronicConfiguration.txt", ios::in);
+	GotoLine(electronicConfigurationDataFile, elementNumber);
+	getline(electronicConfigurationDataFile, electronicConfiguration);
+	electronicConfigurationDataFile.close();
 
-    // to get elementSymbol
-    fstream elementSymbolDataFile;
-    elementSymbolDataFile.open("data/elementSymbol.txt", ios::in);
-    GotoLine(elementSymbolDataFile, elementNumber);
-    getline(elementSymbolDataFile, elementSymbol);
-    elementSymbolDataFile.close();
+	// to get elementSymbol
+	fstream elementSymbolDataFile;
+	elementSymbolDataFile.open("data/elementSymbol.txt", ios::in);
+	GotoLine(elementSymbolDataFile, elementNumber);
+	getline(elementSymbolDataFile, elementSymbol);
+	elementSymbolDataFile.close();
 
-    // to get group
-    fstream groupDataFile;
-    groupDataFile.open("data/group.txt", ios::in);
-    GotoLine(groupDataFile, elementNumber);
-    getline(groupDataFile, group);
-    groupDataFile.close();
+	// to get group
+	fstream groupDataFile;
+	groupDataFile.open("data/group.txt", ios::in);
+	GotoLine(groupDataFile, elementNumber);
+	getline(groupDataFile, group);
+	groupDataFile.close();
 
-    // to get melting point
-    fstream meltingPointDataFile;
-    meltingPointDataFile.open("data/meltingPoint.txt", ios::in);
-    GotoLine(meltingPointDataFile, elementNumber);
-    getline(meltingPointDataFile, meltingPoint);
-    meltingPointDataFile.close();
+	// to get melting point
+	fstream meltingPointDataFile;
+	meltingPointDataFile.open("data/meltingPoint.txt", ios::in);
+	GotoLine(meltingPointDataFile, elementNumber);
+	getline(meltingPointDataFile, meltingPoint);
+	meltingPointDataFile.close();
 
-    // to add units
-    atomicWeight += " amu";
-    boilingPoint += " C";
-    meltingPoint += " C";
+	// to add units
+	atomicWeight += " amu";
+	boilingPoint += " C";
+	meltingPoint += " C";
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     // save result to file - module
@@ -175,112 +164,93 @@ NOINPUT:
     outputDataFile.open("output.txt", ios::out);
     outputDataFile << "+-------------------------------+------------------------------------+" << endl;
     outputDataFile << "|    Element Name               |    " << elementName;
-    for (int i = 0; i < (32 - elementName.length()); i++)
-    {
+    for(int i = 0 ; i < (32 - elementName.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
     outputDataFile << "+-------------------------------+------------------------------------+" << endl;
     outputDataFile << "|    Atomic Number              |    " << atomicNumber;
-    for (int i = 0; i < (32 - atomicNumber.length()); i++)
-    {
+    for(int i = 0 ; i < (32 - atomicNumber.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
     outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Atomic Radius              |    " << atomicRadius;
-    for (int i = 0; i < (32 - atomicRadius.length()); i++)
-    {
+	outputDataFile << "|    Atomic Radius              |    " << atomicRadius;
+    for(int i = 0 ; i < (32 - atomicRadius.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Atomic Weight              |    " << atomicWeight;
-    for (int i = 0; i < (32 - atomicWeight.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Atomic Weight              |    " << atomicWeight;
+	for(int i = 0 ; i < (32 - atomicWeight.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Block                      |    " << block;
-    for (int i = 0; i < (32 - block.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Block                      |    " << block;
+	for(int i = 0 ; i < (32 - block.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Boiling Point              |    " << boilingPoint;
-    for (int i = 0; i < (32 - boilingPoint.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Boiling Point              |    " << boilingPoint;
+	for(int i = 0 ; i < (32 - boilingPoint.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Density                    |    " << density;
-    for (int i = 0; i < (32 - density.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Density                    |    " << density;
+	for(int i = 0 ; i < (32 - density.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Electronegativity          |    " << electroNegativity;
-    for (int i = 0; i < (32 - electroNegativity.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Electronegativity          |    " << electroNegativity;
+	for(int i = 0 ; i < (32 - electroNegativity.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Electronic Configuration   |    " << electronicConfiguration;
-    for (int i = 0; i < (32 - electronicConfiguration.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Electronic Configuration   |    " << electronicConfiguration;
+	for(int i = 0 ; i < (32 - electronicConfiguration.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Element Symbol             |    " << elementSymbol;
-    for (int i = 0; i < (32 - elementSymbol.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Element Symbol             |    " << elementSymbol;
+	for(int i = 0 ; i < (32 - elementSymbol.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Group                      |    " << group;
-    for (int i = 0; i < (32 - group.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Group                      |    " << group;
+	for(int i = 0 ; i < (32 - group.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|    Melting Point              |    " << meltingPoint;
-    for (int i = 0; i < (32 - meltingPoint.length()); i++)
-    {
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|    Melting Point              |    " << meltingPoint;
+	for(int i = 0 ; i < (32 - meltingPoint.length()) ; i++){
         outputDataFile << " ";
     }
     outputDataFile << "|" << endl;
-    outputDataFile << "+-------------------------------+------------------------------------+" << endl;
-    outputDataFile << "|           Modern Periodic Table - A Project By HyphenSaad          |" << endl;
-    outputDataFile << "|                Saad Mansoor - 70075954 - BSCS-I / C1               |" << endl;
-    outputDataFile << "+--------------------------------------------------------------------+" << endl;
+	outputDataFile << "+-------------------------------+------------------------------------+" << endl;
+	outputDataFile << "|           Modern Periodic Table - A Project By HyphenSaad          |" << endl;
+	outputDataFile << "|                Saad Mansoor - 70075954 - BSCS-I / C1               |" << endl;
+	outputDataFile << "+--------------------------------------------------------------------+" << endl;
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     // result output - module
     int width = 0;
 
     // to calculate result window width
-    if (electronicConfiguration.length() < 31 && electronicConfiguration.length() > 20)
-    {
+    if(electronicConfiguration.length() < 31 && electronicConfiguration.length() > 20){
         width = electronicConfiguration.length() * 8;
-    }
-    else if (electronicConfiguration.length() < 21 && electronicConfiguration.length() > 10)
-    {
+    } else if(electronicConfiguration.length() < 21 && electronicConfiguration.length() > 10){
         width = electronicConfiguration.length() * 11;
-    }
-    else if (electronicConfiguration.length() < 11 && electronicConfiguration.length() > 5)
-    {
+    } else if(electronicConfiguration.length() < 11 && electronicConfiguration.length() > 5){
         width = electronicConfiguration.length() * 16;
-    }
-    else
-    {
+    } else {
         width = electronicConfiguration.length() * 27;
     }
 
@@ -298,7 +268,7 @@ NOINPUT:
     setcolor(DARKGRAY);
     setbkcolor(LIGHTGRAY);
     settextstyle(10, 0, 4);
-    outtextxy((getmaxx() - (elementName.length() * 22)) / 2, 8, &elementName[0]);
+    outtextxy((getmaxx() - (elementName.length() * 22))/2 , 8, &elementName[0]);
 
     // for table data
     settextstyle(11, 0, 1);
@@ -349,32 +319,23 @@ NOINPUT:
     outtextxy(50, 250, "Melting Point");
     outtextxy(400, 250, &meltingPoint[0]);
 
-// to terminate result screen
-KILL:
-    char kill = getch();
-    if (kill == 88 || kill == 120)
-    {
+    // to terminate result screen
+    KILL: char kill = getch();
+    if(kill == 88 || kill == 120){
         closegraph(CURRENT_WINDOW);
-    }
-    else
-    {
+    } else {
         goto KILL;
     }
 
-// to terminate the program
-B:
+    // to terminate the program
+    B:
     cout << "> Do yo want to exit? (Y/N)\t";
     cin >> option;
-    if (option == 'N' || option == 'n')
-    {
+    if(option == 'N' || option == 'n'){
         goto RERUN;
-    }
-    else if (option == 'Y' || option == 'y')
-    {
+    } else if(option == 'Y' || option == 'y'){
         exit(1);
-    }
-    else
-    {
+    } else {
         cout << "> Invalid! Command..." << endl;
         goto B;
     }
